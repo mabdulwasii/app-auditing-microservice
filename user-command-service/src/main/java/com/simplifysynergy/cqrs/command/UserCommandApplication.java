6ebff6bb3f3aa2cfb1ad44e16df558d1884f36bb
Tunji Abdulwasiu Moronkola <mabdulwasii@gmail.com>
2023-05-28 02:08:34 +0100

    feat: refactoring the ms codes

--- user-command-service/src/main/java/com/simplifysynergy/cqrs/command/UserCommandApplication.java
+++ user-command-service/src/main/java/com/simplifysynergy/cqrs/command/UserCommandApplication.java
@@ -3,9 +3,6 @@ package com.simplifysynergy.cqrs.command;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,12 +10,6 @@ public class UserCommandApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserCommandApplication.class, args);
-	}
-
-	@Bean
-	@LoadBalanced
-	public WebClient.Builder loadBalancedWebClientBuilder() {
-		return WebClient.builder();
 	}
 	
 }
