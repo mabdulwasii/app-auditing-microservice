a27d5d29cb7e19a032c7e5db6289468ede6c24d9
Tunji Abdulwasiu Moronkola <mabdulwasii@gmail.com>
2023-05-28 01:57:51 +0100

    feat: rename microservices

--- account-command-service/src/main/java/com/simplifysynergy/cqrs/account/repository/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.simplifysynergy.cqrs.account.repository;
-
-import com.simplifysynergy.cqrs.domain.Account;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface AccountRepository extends ReactiveCrudRepository<Account, String> {}
