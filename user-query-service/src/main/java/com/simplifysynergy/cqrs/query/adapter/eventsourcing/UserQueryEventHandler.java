77d2758ccc7427da0ab739f6af36c95b718db444
Tunji Abdulwasiu Moronkola <mabdulwasii@gmail.com>
2023-05-29 00:15:02 +0100

    feat: optimize user query event listener

--- user-query-service/src/main/java/com/simplifysynergy/cqrs/query/adapter/eventsourcing/UserQueryEventHandler.java
+++ user-query-service/src/main/java/com/simplifysynergy/cqrs/query/adapter/eventsourcing/UserQueryEventHandler.java
@@ -2,66 +2,57 @@ package com.simplifysynergy.cqrs.query.adapter.eventsourcing;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.simplifysynergy.cqrs.common.domain.User;
-import com.simplifysynergy.cqrs.common.enumeration.EventType;
-import com.simplifysynergy.cqrs.common.event.UserCreateEvent;
-import com.simplifysynergy.cqrs.common.event.UserDeleteEvent;
-import com.simplifysynergy.cqrs.common.event.UserUpdateEvent;
+import com.simplifysynergy.cqrs.common.event.Event;
 import com.simplifysynergy.cqrs.query.usecase.UserQueryUseCase;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
-
 @Component
 @Slf4j
+@AllArgsConstructor
 public class UserQueryEventHandler {
+
     private final ObjectMapper objectMapper;
     private final UserQueryUseCase useCase;
 
-    public UserQueryEventHandler(ObjectMapper objectMapper, UserQueryUseCase useCase) {
-        this.objectMapper = objectMapper;
-        this.useCase = useCase;
-    }
-
-    @KafkaListener(topics = "${spring.kafka.transferTopic}",  groupId = "${spring.kafka.groupId}",
+    @KafkaListener(topics = "${spring.kafka.transferTopic}", groupId = "${spring.kafka.groupId}",
             containerFactory = "kafkaListenerContainerFactory")
     public Mono<Void> consumeAuditEvent(ConsumerRecord<String, String> consumerRecord) {
         log.info("Consuming audit event {} ", consumerRecord);
+
+        String message = consumerRecord.value();
+        Event event = null;
         try {
-            String message = consumerRecord.value();
-            Map<String, String> userEvent = new ObjectMapper().readValue(message, Map.class);
-
-            if (userEvent.get("type").equals(EventType.CREATE.name())) {
-                UserCreateEvent event = objectMapper.readValue(message, UserCreateEvent.class);
-                User user = event.user();
-
+            event = objectMapper.readValue(message, Event.class);
+        } catch (Exception e) {
+            throw new IllegalStateException("Could not parse event message");
+        }
+        log.info("processing event {} ", event);
+        User user = event.getUser();
+        switch (event.getType()) {
+            case CREATE -> {
                 return useCase.save(user)
                         .mapNotNull(savedUser -> {
                             log.info("Save user {} in user readonly db", savedUser);
                             return savedUser;
                         }).then();
-            } else if (userEvent.get("type").equals(EventType.DELETE.name())) {
-                UserDeleteEvent userDeleteEvent = objectMapper.readValue(message, UserDeleteEvent.class);
-                User user = userDeleteEvent.user();
-                log.info("User deleted with id {} ", user.getId());
-                return useCase.deleteById(user.getId());
-            } else {
-                UserUpdateEvent event = objectMapper.readValue(message, UserUpdateEvent.class);
-                User user = event.user();
+            }
+            case UPDATE -> {
                 return useCase.update(user)
                         .map(updatedUser -> {
                             log.info("Updated user {} in user readonly db", updatedUser);
                             return updatedUser;
                         }).then();
             }
-        } catch (Exception e) {
-            log.error("Error while handling message", e);
-            throw new KafkaException(e.getMessage(),e);
+            case DELETE -> {
+                log.info("Delete user with id {} ", user.getId());
+                return useCase.deleteById(user.getId());
+            }
+            default -> throw new IllegalStateException("Invalid event type: " + event.getType());
         }
     }
-
 }
